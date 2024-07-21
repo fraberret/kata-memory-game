f16b9018d740382297441b1527194dd43c25b2fb
fraberret <f.berretta02@gmail.com>
2024-07-21 18:27:40 +0200

    create an array shuffle it and add on page

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-/* Generare 12 caselle */
-for (let i = 1; i <= 12; i++) {
-    document.getElementById('row').insertAdjacentHTML('beforeend', ` <div class="col-3">
-                <img src="./assets/images/back.png" alt=""></div>`)
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const couples = [
+        './assets/images/alien.png', './assets/images/alien.png',
+        './assets/images/rocket.png','./assets/images/rocket.png',
+        './assets/images/duck.png','./assets/images/duck.png',
+        './assets/images/spaceship.png','./assets/images/spaceship.png',
+        './assets/images/tiktac.png','./assets/images/tiktac.png',
+        './assets/images/bug.png','./assets/images/bug.png'
+    ];
+
+    function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
+
+    shuffle(couples);
+    console.log(couples);
+
+    const rowElement = document.getElementById("row");
+    if (rowElement) {
+        couples.forEach(element => {
+            const colDiv = document.createElement('div');
+            colDiv.className = 'col-3';
+            const img = document.createElement('img');
+            img.src = element;
+            img.alt = '';
+            colDiv.appendChild(img);
+            rowElement.appendChild(colDiv);
+        });
+    } else {
+        console.error('Element with id "row" not found.');
+    }
+});
